36034dd2a88a3ecdcfefe7d4ebbef4d3640033df
Sandeep Sharma <1949sandeepsharma52@gmail.com>
2023-12-19 13:48:49 +0545

    added react-draggable for sticky design

--- src/components/KanbanBoard/BoardColumn.tsx
+++ src/components/KanbanBoard/BoardColumn.tsx
@@ -6,18 +6,46 @@ import { Droppable } from "react-beautiful-dnd";
 
 const BoardColumn = ({ title, issues }: { title: string; issues: any }) => {
 	return (
-		<div className="min-w-[25%] flex flex-col gap-4">
+		<div className="min-w-[324px] w-[324px] flex flex-col gap-4">
 			<div className="text-lg font-semibold bg-gray-50 p-2 rounded-t-lg">
 				{title}
 			</div>
-			<div className="gap-2 flex flex-wrap bg-gray-50 p-2 h-full overflow-y-auto overflow-x-hidden">
-				<Droppable droppableId={title}>
-					{(provided, snapshot) => (
-						<div
-							className="w-full"
-							ref={provided.innerRef}
-							{...provided.droppableProps}
-						>
+			{/* <Droppable droppableId={title}> */}
+			{/* {(provided, snapshot) => ( */}
+			<div className="w-full">
+				<div className="gap-2 bg-gray-50 p-2 min-h-[500px] overflow-y-auto overflow-x-hidden">
+					{issues?.length > 0
+						? issues.map((item: any, index: any) => (
+								<Task
+									key={item.id}
+									color={
+										colors[
+											Math.floor(
+												Math.random() * colors.length
+											)
+										]
+									}
+									task={item.name}
+									storyPoints={item.storyPoint}
+									avatar={item.assignee?.image}
+									index={index}
+									id={`${item.id}`}
+								/>
+						  ))
+						: null}
+					{/* {provided.placeholder} */}
+				</div>
+			</div>
+			{/* )} */}
+			{/* </Droppable> */}
+			{/* <Droppable droppableId={title}>
+				{(provided, snapshot) => (
+					<div
+						className="w-full"
+						ref={provided.innerRef}
+						{...provided.droppableProps}
+					>
+						<div className="gap-2 bg-gray-50 p-2 min-h-[500px] overflow-y-auto overflow-x-hidden">
 							{issues?.length > 0
 								? issues.map((item: any, index: any) => (
 										<Task
@@ -40,9 +68,9 @@ const BoardColumn = ({ title, issues }: { title: string; issues: any }) => {
 								: null}
 							{provided.placeholder}
 						</div>
-					)}
-				</Droppable>
-			</div>
+					</div>
+				)}
+			</Droppable> */}
 		</div>
 	);
 };
--- src/components/KanbanBoard/Task.tsx
+++ src/components/KanbanBoard/Task.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Image from "next/image";
-import { Draggable } from "react-beautiful-dnd";
+import Draggable from "react-draggable";
 
 const Task = ({
 	color,
@@ -20,35 +20,30 @@ const Task = ({
 }) => {
 	task = task.length > 40 ? task.slice(0, 40) + "..." : task;
 	return (
-		<Draggable draggableId={`${id}`} index={index} key={id}>
-			{(provided, snapshot) => (
-				<div
-					className={`flex flex-col justify-between shadow-md shadow-gray-200 p-2 min-w-[48%] max-w-[48%] h-28 ${color}`}
-					ref={provided.innerRef}
-					{...provided.draggableProps}
-					{...provided.dragHandleProps}
-				>
-					<div className="text-sm font-semibold">{task}</div>
-					<div className="flex gap-2 justify-end items-end">
-						{storyPoints >= 0 && (
-							<div className="text-xs font-semibold text-gray-400 h-6 w-6 bg-white rounded-full flex items-center justify-center">
-								{storyPoints}
-							</div>
-						)}
-						{avatar && (
-							<div className="text-xs font-semibold text-gray-400 h-6 w-6 bg-white rounded-full flex items-center justify-center">
-								<Image
-									src={avatar}
-									alt="plus"
-									width={24}
-									height={24}
-									className="rounded-full"
-								/>
-							</div>
-						)}
-					</div>
+		<Draggable key={id}>
+			<div
+				className={`flex flex-col justify-between shadow-md shadow-gray-200 p-2 min-w-[150px] max-w-[150px] h-28 ${color}`}
+			>
+				<div className="text-sm font-semibold">{task}</div>
+				<div className="flex gap-2 justify-end items-end">
+					{storyPoints >= 0 && (
+						<div className="text-xs font-semibold text-gray-400 h-6 w-6 bg-white rounded-full flex items-center justify-center">
+							{storyPoints}
+						</div>
+					)}
+					{avatar && (
+						<div className="text-xs font-semibold text-gray-400 h-6 w-6 bg-white rounded-full flex items-center justify-center">
+							<Image
+								src={avatar}
+								alt="plus"
+								width={24}
+								height={24}
+								className="rounded-full"
+							/>
+						</div>
+					)}
 				</div>
-			)}
+			</div>
 		</Draggable>
 	);
 };
